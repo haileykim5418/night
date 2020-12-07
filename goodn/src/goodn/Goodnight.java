22e97db8cc77a5f2a58ea1263d3566b0afce7cce
haileykim5418 <75109901+haileykim5418@users.noreply.github.com>
2020-12-07 21:07:14 +0900

    morning
    아침!

--- goodn/src/goodn/Goodnight.java
+++ goodn/src/goodn/Goodnight.java
@@ -5,6 +5,7 @@ public class Goodnight {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("goodnight");
+		System.out.println("goodmorning");
 	}
 
 }
