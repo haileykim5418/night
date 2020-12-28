6ebfb86a7ec706e4d9c4f13427bdadf97752d8ac
haileykim5418 <rlarkgus5418@naver.com>
2020-12-28 14:28:48 +0900

    1111

---
+++ java_work/c1228/src/c1228/Arr1228.java
@@ -0,0 +1,17 @@
+package c1228;
+
+import java.util.Scanner;
+
+public class Arr1228 {
+
+	public static void main(String[] args) {
+		
+System.out.println(3+1);
+int a =0;
+Scanner s = new Scanner(System.in);
+System.out.println("입력하소");
+a= s.nextInt();
+
+	}
+
+}
