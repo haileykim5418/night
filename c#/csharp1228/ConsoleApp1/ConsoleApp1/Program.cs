83992c06000f45f6b436d346ded5506656809ba9
haileykim5418 <rlarkgus5418@naver.com>
2020-12-28 13:59:23 +0900

    1228

---
+++ c#/csharp1228/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ab = new int[100];
+            int ex = int.Parse(Console.ReadLine());
+            int[] abcd = new int[ex];
+            Console.WriteLine(ex);
+        }
+    }
+}
